f38b756d009235b0e0da827367d1d399809e0607
TakaharaYota <takahara.yota@gmail.com>
2022-10-01 16:40:02 +0900

    haraheta

---
+++ practice/mnist_show.py
@@ -0,0 +1,10 @@
+# import sys ,os
+# from xml.dom import minicompat
+# sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+# import numpy as np
+# from dataset.mnist import load_mnist
+# from PIL import import Image 
+
+# def img_show(img):
+#     pil_img=Image.fromarray(np.uint8(img))
+#     pil_img.show()
